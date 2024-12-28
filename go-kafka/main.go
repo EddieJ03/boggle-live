4e072889b07dfa2466944dcbc0e266620a479b2b
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-12-28 11:49:39 -0800

    add WaitGroups to ensure goroutines finish everything they need before closing channel

--- go-kafka/main.go
+++ go-kafka/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"strings"
+	"sync"
 
 	"github.com/segmentio/kafka-go"
 
@@ -19,36 +21,7 @@ type WSClient struct {
 	Conn           *websocket.Conn
 	UniqueNumber   int
 	MessageChan    chan map[string]interface{}
-	CancelCtx 	   context.CancelFunc 
-}
-
-func (c * WSClient) newSpectator(ctx context.Context, topic string) {
-    r := kafka.NewReader(kafka.ReaderConfig{
-        Brokers:   []string{"localhost:9094"},
-        Topic:     topic,
-        Partition: 0,
-        MaxBytes:  10e6, 
-    })
-	defer r.Close()
-
-    for {
-        select {
-        case <-ctx.Done():
-            return // Exit gracefully if context is canceled
-        default:
-            m, err := r.ReadMessage(ctx)
-
-            c.MessageChan <- map[string]interface{}{
-                "type":    "message",
-                "message": string(m.Value),
-				"topic": topic,
-            }
-			
-            if err != nil || strings.Contains(string(m.Value), "GAME OVER!") {
-                return // Exit on error or "GAME OVER!"
-            }
-        }
-    }
+	WaitGroup      sync.WaitGroup
 }
 
 func topicExists(topic string) (bool, error) {
@@ -71,24 +44,81 @@ func topicExists(topic string) (bool, error) {
 			return true, nil
 		}
 	}
+
 	return false, nil
 }
+
+func (c * WSClient) newSpectator(ctx context.Context, topic string) {
+	fmt.Printf("NEW spectator for topic %s \n", topic)
+
+    r := kafka.NewReader(kafka.ReaderConfig{
+        Brokers:   []string{"localhost:9094"},
+        Topic:     topic,
+        Partition: 0,
+        MaxBytes:  10e6, 
+    })
+
+	c.WaitGroup.Add(1)
+
+	
+	defer fmt.Printf("FINISHED spectator for topic %s \n", topic)
+	defer c.WaitGroup.Done()
+	defer r.Close()
+
+    for {
+        select {
+        case <-ctx.Done():
+			fmt.Println("EXITING saw context finished")
+            return // Exit gracefully if context is canceled
+        default:
+            m, err := r.ReadMessage(ctx)
+			
+            if err != nil {
+				if errors.Is(err, context.Canceled) {
+					fmt.Println("EXITING saw context CANCELLED ERROR")
+					return
+				}
+
+				c.MessageChan <- map[string]interface{}{
+					"type":    "error",
+					"message": "Failed to read from game " + topic + "!",
+				}
+
+                return // Exit on error
+            }
+
+            c.MessageChan <- map[string]interface{}{
+                "type":    "message",
+                "message": string(m.Value),
+				"topic": topic,
+            }
+
+			if strings.Contains(string(m.Value), "GAME OVER!") { // Exit on "GAME OVER!"
+				return
+			}
+        }
+    }
+}
+
 
 func (c *WSClient) HandleClient() {
     defer c.Conn.Close()
 	fmt.Printf("%d connected\n", c.UniqueNumber)
 
+	ctx, cancel := context.WithCancel(context.Background())
+
 	c.Conn.SetCloseHandler(func(code int, text string) error {
 		fmt.Printf("%d closed so disconnected\n", c.UniqueNumber)
+		cancel()
+		c.WaitGroup.Wait()
 		close(c.MessageChan)
 		return nil
 	})
 
-	ctx, cancel := context.WithCancel(context.Background())
-    c.CancelCtx = cancel
 
 	// pipe all messages through a dedicated channel (best if message order is needed, but not necessary for my implementation)
 	go func() {
+		fmt.Printf("starting infinite read from message channel\n")
         for message := range c.MessageChan {
             if err := c.Conn.WriteJSON(message); err != nil {
                 log.Println("WebSocket write error:", err)
@@ -96,50 +126,53 @@ func (c *WSClient) HandleClient() {
                 break
             }
         }
+		fmt.Printf("finished infinite read from message channel\n")
     }()
 
 	for {
-		var data map[string]interface{}
+		select {
+		case <-ctx.Done():
+			fmt.Printf("%d context canceled so disconnecting\n", c.UniqueNumber)
+        	return
+		default:
+			var data map[string]interface{}
 
-		err := c.Conn.ReadJSON(&data)
-		if err != nil {
-			fmt.Println(err)
-
-			fmt.Printf("%d error so disconnected\n", c.UniqueNumber)
-
-			break
-		}
-
-		msgType, ok := data["type"].(string)
-		if !ok {
-			fmt.Printf("%s is invalid type for message Type\n", msgType)
-			continue
-		}
-
-
-		fmt.Println("topic: " + data["topic"].(string))
-		fmt.Println("messageType: " + msgType)
-
-		switch msgType {
-		case "newSpectator":
-			exists, err := topicExists(data["topic"].(string))
-
+			err := c.Conn.ReadJSON(&data)
 			if err != nil {
-				fmt.Printf("Error checking topic existence: %v", err)
+				fmt.Printf("%d error so disconnected\n", c.UniqueNumber)
+
+				cancel()
+
+				return
+			}
+
+			msgType, ok := data["type"].(string)
+			if !ok {
+				fmt.Printf("%s is invalid type for message Type\n", msgType)
 				continue
 			}
 
-			if exists {
-				go c.newSpectator(ctx, data["topic"].(string))
-			} else {
-				c.Conn.WriteJSON(map[string]interface{}{
-					"type":   "nonexistent",
-					"topic":   data["topic"].(string),
-				})
+			switch msgType {
+			case "newSpectator":
+				exists, err := topicExists(data["topic"].(string))
+
+				if err != nil {
+					fmt.Printf("Error checking topic existence: %v", err)
+					continue
+				}
+
+				if exists {
+					go c.newSpectator(ctx, data["topic"].(string))
+				} else {
+					c.Conn.WriteJSON(map[string]interface{}{
+						"type":   "error",
+						"message": "Game " + data["topic"].(string) + " does not exist!",
+					})
+				}
+				
+			default:
+				continue
 			}
-			
-		default:
-			continue
 		}
 	}
 }
